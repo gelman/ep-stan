e17e7121b0888038181aefd720ec30a9acc5e032
Tuomas Sivula <tsivula@users.noreply.github.com>
2018-01-17 18:21:29 +0200

    Update plots for the paper (experiment with varying siter)

--- experiment/plot_paper_ll.py
+++ experiment/plot_paper_ll.py
@@ -1,4 +1,4 @@
-"""Plot script for the paper."""
+"""Plot script for the paper with log likelihood."""
 
 # Licensed under the 3-clause BSD license.
 # http://opensource.org/licenses/BSD-3-Clause
@@ -73,9 +73,9 @@ plt.style.use(gray_background)
 
 MODEL_NAME = 'm4b'
 KS = (2, 4, 8, 16, 32, 64)
-EP_ID = 'siter400'
+EP_ID = 'siter200'
 CONS_ID = ''
-FULL_N = 6
+FULL_N = 7
 
 
 
@@ -109,6 +109,11 @@ target_file = np.load('./results/target_{}.npz'.format(MODEL_NAME))
 m_target = target_file['m_target']
 S_target = target_file['S_target']
 target_file.close()
+
+# Load target samples
+target_samp_file = np.load('./results/target_samp_{}.npz'.format(MODEL_NAME))
+samp_target = target_samp_file['samp_target']
+target_samp_file.close()
 
 # Load EP result file
 m_s_ep_s = []
@@ -169,40 +174,104 @@ res_f_file.close()
 
 
 # -------------------
-#   Calc MSE and KL
+#   Calc measures
 # -------------------
 
 dphi = m_target.shape[0]
 sum_log_diag_cho_S0 = np.sum(np.log(np.diag(cho_factor(S_target)[0])))
+triu_inds = np.triu_indices_from(S_target)
 
 # EP
 mse_ep_s = []
+msestd_ep_s = []
 kl_ep_s = []
+ll_ep_s = []
 for m_s_ep, S_s_ep in zip(m_s_ep_s, S_s_ep_s):
+
     mse_ep = np.mean((m_s_ep - m_target)**2, axis=1)
+    mse_ep_s.append(mse_ep)
+
+    msestd_ep = np.mean(
+        np.square(
+            np.sqrt(np.diagonal(S_s_ep, axis1=1, axis2=2)) -
+            np.sqrt(np.diag(S_target))
+        ),
+        axis=1
+    )
+    msestd_ep_s.append(msestd_ep)
+
     kl_ep = np.empty(len(m_s_ep))
     for i in range(len(m_s_ep)):
         kl_ep[i] = kl_mvn(
             m_target, S_target, m_s_ep[i], S_s_ep[i], sum_log_diag_cho_S0)
-    mse_ep_s.append(mse_ep)
     kl_ep_s.append(kl_ep)
+
+    ll_ep = np.zeros(len(m_s_ep))
+    for i in range(len(m_s_ep)):
+        ll_ep[i] = np.sum(stats.multivariate_normal.logpdf(
+            samp_target, mean=m_s_ep[i], cov=S_s_ep[i]))
+    ll_ep_s.append(ll_ep)
+
 # consensus
 mse_cons_s = []
+msestd_cons_s = []
 kl_cons_s = []
+ll_cons_s = []
 for m_s_cons, S_s_cons in zip(m_s_cons_s, S_s_cons_s):
+
     mse_cons = np.mean((m_s_cons - m_target)**2, axis=1)
+    mse_cons_s.append(mse_cons)
+
+    msestd_cons = np.mean(
+        np.square(
+            np.sqrt(np.diagonal(S_s_cons, axis1=1, axis2=2)) -
+            np.sqrt(np.diag(S_target))
+        ),
+        axis=1
+    )
+    msestd_cons_s.append(msestd_cons)
+
     kl_cons = np.empty(len(m_s_cons))
     for i in range(len(m_s_cons)):
         kl_cons[i] = kl_mvn(
             m_target, S_target, m_s_cons[i], S_s_cons[i], sum_log_diag_cho_S0)
-    mse_cons_s.append(mse_cons)
     kl_cons_s.append(kl_cons)
+
+    ll_cons = np.zeros(len(m_s_cons))
+    for i in range(len(m_s_cons)):
+        ll_cons[i] = np.sum(stats.multivariate_normal.logpdf(
+            samp_target, mean=m_s_cons[i], cov=S_s_cons[i]))
+    ll_cons_s.append(ll_cons)
+
 # full
+
 mse_full = np.mean((m_s_full - m_target)**2, axis=1)
+
+msestd_full = np.mean(
+    np.square(
+        np.sqrt(np.diagonal(S_s_full, axis1=1, axis2=2)) -
+        np.sqrt(np.diag(S_target))
+    ),
+    axis=1
+)
+
 kl_full = np.full(len(m_s_full), np.nan)
 for i in range(len(m_s_full)):
     kl_full[i] = kl_mvn(
         m_target, S_target, m_s_full[i], S_s_full[i], sum_log_diag_cho_S0)
+
+ll_full = np.zeros(len(m_s_full))
+for i in range(len(m_s_full)):
+    ll_full[i] = np.sum(stats.multivariate_normal.logpdf(
+        samp_target, mean=m_s_full[i], cov=S_s_full[i]))
+
+
+# ll negative
+for ll_ep in ll_ep_s:
+    ll_ep *= -1
+for ll_cons in ll_cons_s:
+    ll_cons *= -1
+ll_full *= -1
 
 
 # ---------
@@ -213,11 +282,11 @@ for i in range(len(m_s_full)):
 K_colors = plt.get_cmap('tab10').colors[:len(KS)]
 
 # --------- time as x-axis
-fig, axes = plt.subplots(2, 1, sharex=True, figsize=figsize4latex(0.99, 1.4))
+fig, axes = plt.subplots(1, 2, figsize=figsize4latex(0.99, 0.9))
 
 lw = 1.0
 
-# MSE
+# MSE mean
 ax = axes[0]
 ax.set_yscale('log')
 # full
@@ -232,7 +301,7 @@ for mse_cons, time_s_cons, k, color in zip(
 # prior label
 ax.axhline(mse_ep_s[0][0], lw=0.5, color='0.65', zorder=1)
 ax.text(
-    x = -0.022,
+    x = -0.05,
     y = mse_ep_s[0][0],
     s = 'prior',
     transform = ax.get_yaxis_transform(),
@@ -241,26 +310,26 @@ ax.text(
 )
 # cosmetics
 ax.minorticks_off()
-# ax.set_xlabel('time (min)')
+ax.set_xlabel('time (min)')
 ax.set_ylabel('MSE')
 
-# KL
+# ll
 ax = axes[1]
 ax.set_yscale('log')
 # full
-ax.plot(time_s_full[:FULL_N]/60, kl_full[:FULL_N], 'k', label='full', lw=lw)
+ax.plot(time_s_full[:FULL_N]/60, ll_full[:FULL_N], 'k', label='full', lw=lw)
 # ep
-for kl_ep, time_s_ep, k, color in zip(kl_ep_s, time_s_ep_s, KS, K_colors):
-    ax.plot(time_s_ep[1:]/60, kl_ep[1:], color=color, label=str(k), lw=lw)
+for ll_ep, time_s_ep, k, color in zip(ll_ep_s, time_s_ep_s, KS, K_colors):
+    ax.plot(time_s_ep[1:]/60, ll_ep[1:], color=color, label=str(k), lw=lw)
 # cons
-for kl_cons, time_s_cons, k, color in zip(
-        kl_cons_s, time_s_cons_s, KS, K_colors):
-    ax.plot(time_s_cons/60, kl_cons, color=color, ls=':', label=str(k), lw=lw)
+for ll_cons, time_s_cons, k, color in zip(
+        ll_cons_s, time_s_cons_s, KS, K_colors):
+    ax.plot(time_s_cons/60, ll_cons, color=color, ls=':', label=str(k), lw=lw)
 # prior label
-ax.axhline(kl_ep_s[0][0], lw=0.5, color='0.65', zorder=1)
+ax.axhline(ll_ep_s[0][0], lw=0.5, color='0.65', zorder=1)
 ax.text(
-    x = -0.022,
-    y = kl_ep_s[0][0],
+    x = -0.05,
+    y = ll_ep_s[0][0],
     s = 'prior',
     transform = ax.get_yaxis_transform(),
     ha = 'right',
@@ -269,7 +338,7 @@ ax.text(
 # cosmetics
 ax.minorticks_off()
 ax.set_xlabel('time (min)')
-ax.set_ylabel('KL')
+ax.set_ylabel('MSE std')
 
 # legend
 legend_k_lines = tuple(
@@ -281,14 +350,14 @@ legend_style_lines = (
     mlines.Line2D([], [], color='gray', ls=':', label='cons.', lw=lw)
 )
 legend_full_lines = (mlines.Line2D([], [], color='k', label='full', lw=lw),)
-axes[0].legend(handles=legend_k_lines+legend_style_lines+legend_full_lines)
+axes[1].legend(handles=legend_k_lines+legend_style_lines+legend_full_lines)
 
 fig.subplots_adjust(
-    top=0.97,
-    bottom=0.05,
-    left=0.11,
-    right=0.96,
-    hspace=0.08,
+    top=0.95,
+    bottom=0.1,
+    left=0.1,
+    right=0.955,
+    hspace=0.2,
     wspace=0.3
 )
 
