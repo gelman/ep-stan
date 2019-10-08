5922723301a689f691118cd4afa0f0f8efe9b820
T. Sivula <tsivula@users.noreply.github.com>
2019-10-08 16:54:43 +0300

    update snep damp

--- experiment/fit.py
+++ experiment/fit.py
@@ -475,8 +475,8 @@ def main(model_name, conf, ret_master=False):
             iters_to_run = conf.iter
 
         if conf.damp is None:
-            # default damp
-            df0 = default_df0(K)
+            # default damp ... no damp
+            df0 = 1.0
         else:
             df0 = conf.damp
 
