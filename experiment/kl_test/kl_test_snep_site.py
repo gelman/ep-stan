f276058508dc21cfc125cf0cc636d9b746fc5521
T. Sivula <tsivula@users.noreply.github.com>
2019-11-11 13:14:38 +0200

    Update

--- experiment/kl_test/kl_test_snep_site.py
+++ experiment/kl_test/kl_test_snep_site.py
@@ -8,6 +8,9 @@ import os, sys
 sys.path.append(os.path.abspath('../models'))
 from common import rand_corr_vine
 
+# LAPACK qr routine
+dgeqrf_routine = linalg.get_lapack_funcs('geqrf')
+
 
 # # figure size for latex
 # # put `\the\textwidth` in the latex content to write it out in the document
@@ -62,7 +65,7 @@ from matplotlib.patches import Patch
 n_dim = 3
 seed = 11
 n_samp = 200
-n_trial = 2000
+n_trial = 400
 n_sites = 6
 
 # true site variance range
@@ -193,6 +196,22 @@ def kl_mvn(m0, S0, m1, S1, sum_log_diag_cho_S0=None):
     )
     return KL_div
 
+def kl_mvn_natural1(m0, S0, m1, Q1):
+    """Calculate KL-divergence for multiv normal distributions
+
+    Calculates KL(p||q), where p ~ N(m0,S0) and q ~ N(m1,S1).
+
+    """
+    dm = m1-m0
+    KL_div = 0.5*(
+        - np.log(linalg.det(S0))
+        - np.log(np.abs(linalg.det(Q1))) # skip imaginary part
+        + np.trace(Q1.dot(S0))
+        + dm.dot(Q1.dot(dm))
+        - len(m0)
+    )
+    return KL_div
+
 
 def rand_cov(n_dim, rng=None):
     if rng is None:
@@ -257,12 +276,22 @@ m_c0_extreme = np.zeros(n_dim)
 
 # test
 if not use_precalculated:
+    mse_init = np.zeros((len(grid_test_site), len(grid_cavity)))
     mse_moment = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
     mse_natural = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
     mse_snep = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    mse_site_init = np.zeros((len(grid_test_site), len(grid_cavity)))
+    mse_site_moment = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    mse_site_natural = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    mse_site_snep = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    kl_init = np.zeros((len(grid_test_site), len(grid_cavity)))
     kl_moment = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
     kl_natural = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
     kl_snep = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    kl_site_init = np.zeros((len(grid_test_site), len(grid_cavity)))
+    kl_site_moment = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    kl_site_natural = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
+    kl_site_snep = np.zeros((len(grid_test_site), len(grid_cavity), n_trial))
     for grid_t_i, multip_t in enumerate(grid_test_site):
 
         # cur site approximatios
@@ -273,6 +302,16 @@ if not use_precalculated:
         m_0 *= 1 - multip_t
         m_0 += multip_t*m_0_extreme
         Q_0, r_0 = invert_params(S_0, m_0)
+
+        # calc initial site mse and kl
+        mse_site_init[grid_t_i, :] = np.mean(
+            (m_0 - m_i_true[:,test_site_i])**2)
+        kl_site_init[grid_t_i, :] = kl_mvn(
+            m_i_true[:,test_site_i],
+            S_i_true[:,:,test_site_i],
+            m_0,
+            S_0,
+        )
 
         for grid_c_i, multip_c in enumerate(grid_cavity):
 
@@ -290,6 +329,16 @@ if not use_precalculated:
             r_g = r_c0 + r_0
             S_g, m_g = invert_params(Q_g, r_g)
 
+            # calc initial mse and kl
+            mse_init[grid_t_i, grid_c_i] = np.mean((m_g - m_t)**2)
+            kl_init[grid_t_i, grid_c_i] = kl_mvn(
+                m_t,
+                S_t,
+                m_g,
+                S_g,
+                sum_log_diag_cho_S0=sum_log_diag_cho_S_t
+            )
+
             # current tilted distribution
             Q_t0 = Q_c0 + Q_i_true[:,:,test_site_i]
             r_t0 = r_c0 + r_i_true[:,test_site_i]
@@ -302,11 +351,23 @@ if not use_precalculated:
                 m_samp = np.mean(samp, axis=0)
                 samp -= m_samp
                 Scatter = samp.T.dot(samp)
+                # calc qr from it
+                qr, _, _, info = dgeqrf_routine(samp)
+                if info:
+                    raise linalg.LinAlgError(
+                        "dgeqrf LAPACK routine failed with error code {}"
+                        .format(info)
+                    )
+                # calc unnormalised new global approx Q and r
+                Q_g_new_un, r_g_new_un = invert_params(
+                    qr[:n_dim,:], m_samp, cho_form=True)
+
 
                 # estim unbias moment
                 S_samp = Scatter / (n_samp-1)
                 # calc mse
-                mse_moment[grid_t_i, grid_c_i, trial_i] = np.mean((m_samp - m_t)**2)
+                mse_moment[grid_t_i, grid_c_i, trial_i] = np.mean(
+                    (m_samp - m_t)**2)
                 # calc KL
                 kl_moment[grid_t_i, grid_c_i, trial_i] = kl_mvn(
                     m_t,
@@ -315,11 +376,29 @@ if not use_precalculated:
                     S_samp,
                     sum_log_diag_cho_S0=sum_log_diag_cho_S_t
                 )
+                # calc new site distribution
+                # Q_g_new, r_g_new = invert_params(S_samp, m_samp)
+                Q_g_new = Q_g_new_un*(n_samp-1)
+                r_g_new = r_g_new_un*(n_samp-1)
+                Q_0_new = Q_g_new - Q_c0
+                r_0_new = r_g_new - r_c0
+                # not necessarily pos def
+                m_0_new = linalg.solve(Q_0_new, r_0_new, assume_a='sym')
+                # calc site mse and kl
+                mse_site_moment[grid_t_i, grid_c_i, trial_i] = np.mean(
+                    (m_0_new - m_i_true[:,test_site_i])**2)
+                kl_site_moment[grid_t_i, grid_c_i, trial_i] = kl_mvn_natural1(
+                    m_i_true[:,test_site_i],
+                    S_i_true[:,:,test_site_i],
+                    m_0_new,
+                    Q_0_new,
+                )
 
                 # estim unbias natural
                 S_samp = Scatter / (n_samp-n_dim-2)
                 # calc mse
-                mse_natural[grid_t_i, grid_c_i, trial_i] = np.mean((m_samp - m_t)**2)
+                mse_natural[grid_t_i, grid_c_i, trial_i] = np.mean(
+                    (m_samp - m_t)**2)
                 # calc KL
                 kl_natural[grid_t_i, grid_c_i, trial_i] = kl_mvn(
                     m_t,
@@ -327,6 +406,23 @@ if not use_precalculated:
                     m_samp,
                     S_samp,
                     sum_log_diag_cho_S0=sum_log_diag_cho_S_t
+                )
+                # calc new site distribution
+                # Q_g_new, r_g_new = invert_params(S_samp, m_samp)
+                Q_g_new = Q_g_new_un*(n_samp-n_dim-2)
+                r_g_new = r_g_new_un*(n_samp-n_dim-2)
+                Q_0_new = Q_g_new - Q_c0
+                r_0_new = r_g_new - r_c0
+                # not necessarily pos def
+                m_0_new = linalg.solve(Q_0_new, r_0_new, assume_a='sym')
+                # calc site mse and kl
+                mse_site_natural[grid_t_i, grid_c_i, trial_i] = np.mean(
+                    (m_0_new - m_i_true[:,test_site_i])**2)
+                kl_site_natural[grid_t_i, grid_c_i, trial_i] = kl_mvn_natural1(
+                    m_i_true[:,test_site_i],
+                    S_i_true[:,:,test_site_i],
+                    m_0_new,
+                    Q_0_new,
                 )
 
                 # estim SNEP
@@ -373,17 +469,36 @@ if not use_precalculated:
                     S_new,
                     sum_log_diag_cho_S0=sum_log_diag_cho_S_t
                 )
+                # calc site mse and kl
+                mse_site_snep[grid_t_i, grid_c_i, trial_i] = np.mean(
+                    (m_0_snep - m_i_true[:,test_site_i])**2)
+                kl_site_snep[grid_t_i, grid_c_i, trial_i] = kl_mvn(
+                    m_i_true[:,test_site_i],
+                    S_i_true[:,:,test_site_i],
+                    m_0_snep,
+                    S_0_snep,
+                )
 
 
     # save
     np.savez(
         'kl_test_snep_res.npz',
+        mse_init=mse_init,
         mse_moment=mse_moment,
         mse_natural=mse_natural,
         mse_snep=mse_snep,
+        mse_site_init=mse_site_init,
+        mse_site_moment=mse_site_moment,
+        mse_site_natural=mse_site_natural,
+        mse_site_snep=mse_site_snep,
+        kl_init=kl_init,
         kl_moment=kl_moment,
         kl_natural=kl_natural,
         kl_snep=kl_snep,
+        kl_site_init=kl_site_init,
+        kl_site_moment=kl_site_moment,
+        kl_site_natural=kl_site_natural,
+        kl_site_snep=kl_site_snep,
         n_dim=n_dim,
         seed=seed,
         n_samp=n_samp,
@@ -403,16 +518,32 @@ if not use_precalculated:
 else:
     # load results
     res_file = np.load('kl_test_snep_res.npz')
+    mse_init = res_file['mse_init']
     mse_moment = res_file['mse_moment']
     mse_natural = res_file['mse_natural']
     mse_snep = res_file['mse_snep']
+    mse_site_init = res_file['mse_site_init']
+    mse_site_moment = res_file['mse_site_moment']
+    mse_site_natural = res_file['mse_site_natural']
+    mse_site_snep = res_file['mse_site_snep']
+    kl_init = res_file['kl_init']
     kl_moment = res_file['kl_moment']
     kl_natural = res_file['kl_natural']
     kl_snep = res_file['kl_snep']
+    kl_site_init = res_file['kl_site_init']
+    kl_site_moment = res_file['kl_site_moment']
+    kl_site_natural = res_file['kl_site_natural']
+    kl_site_snep = res_file['kl_site_snep']
     res_file.close()
 
 
 
+
+def remove_frame(ax):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
 
 
 # # fig, axes = plt.subplots(3, 1, sharex=True, figsize=figsize4latex(0.98, 0.85))
@@ -499,13 +630,6 @@ else:
 # # plt.savefig("fig_test_snep.pgf")
 
 
-def remove_frame(ax):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-
-
 # fig, axes = plt.subplots(
 #     len(grid_test_site), len(grid_cavity),
 #     sharex=True, figsize=(10,9)
@@ -566,12 +690,23 @@ def remove_frame(ax):
 
 
 
+# calc change
+mse_change_natural = mse_natural - mse_init[:,:,None]
+mse_change_snep = mse_snep - mse_init[:,:,None]
+kl_change_natural = kl_natural - kl_init[:,:,None]
+kl_change_snep = kl_snep - kl_init[:,:,None]
+# calc site change
+mse_schange_natural = mse_site_natural - mse_site_init[:,:,None]
+mse_schange_snep = mse_site_snep - mse_site_init[:,:,None]
+kl_schange_natural = kl_site_natural - kl_site_init[:,:,None]
+kl_schange_snep = kl_site_snep - kl_site_init[:,:,None]
+
 
 
 
 for data_n, data_s, fig_name in zip(
-        (mse_natural, kl_natural),
-        (mse_snep, kl_snep),
+        (mse_schange_natural, kl_schange_natural),
+        (mse_schange_snep, kl_schange_snep),
         ('mse', 'kl')):
 
     # fig, axes = plt.subplots(
@@ -592,15 +727,21 @@ for data_n, data_s, fig_name in zip(
             ax.bar(
                 [-0.5, 0.5],
                 [n_500, s_500],
-                yerr=[
-                    [n_500-n_025, s_500-s_025],
-                    [n_975-n_500, s_975-s_500]
-                ],
+                # yerr=[
+                #     [n_500-n_025, s_500-s_025],
+                #     [n_975-n_500, s_975-s_500]
+                # ],
                 color=['C0', 'C1'],
 
             )
-            # ax.set_yticks([0, max(n_500, s_500)])
-            ax.set_yticks([0, ax.get_ylim()[1]])
+            # ylims = ax.get_ylim()
+            # if ylims[0] < 0 and ylims[1] > 0:
+            #     ax.set_yticks([ax.get_ylim()[0], 0, ax.get_ylim()[1]])
+            # elif ylims[0] < 0:
+            #     ax.set_yticks([ax.get_ylim()[0], 0])
+            # else:
+            #     ax.set_yticks([0, ax.get_ylim()[1]])
+            ax.set_yticks(ax.get_ylim())
             ax.tick_params('y', colors='C0')
             if True:
                 ax.spines['top'].set_visible(False)
@@ -608,7 +749,7 @@ for data_n, data_s, fig_name in zip(
             else:
                 remove_frame(ax)
             ax.yaxis.set_major_formatter(FormatStrFormatter('%.2g'))
-            ax.tick_params(axis='both', which='both', length=0, labelsize=12)
+            ax.tick_params(axis=u'both', which=u'both', length=0)
     for ax in axes[-1,:]:
         ax.set_xticks([])
         # ax.set_xticks([-0.5, 0.5])
